41a1cbad616a3eec0c677748a829964bc8d93ea9
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2022-01-16 14:37:40 +0100

    Format

--- pg-bao/postgres-bao-initial-start.sh
+++ pg-bao/postgres-bao-initial-start.sh
@@ -19,6 +19,7 @@ createdb $(whoami)
 echo ".. Setting up BAO"
 echo "shared_preload_libraries = 'pg_bao'" >> $(pwd)/build/data/postgresql.conf
 sed -i "s/^shared_buffers.*/shared_buffers = 12GB/" $(pwd)/build/data/postgresql.conf
+
 echo ".. Restarting to apply changes"
 pg_ctl restart -D $(pwd)/build/data
 
