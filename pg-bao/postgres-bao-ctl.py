b43e977ac963559d332d62cef3a469f12f960fdd
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2022-01-22 12:08:54 +0100

    Overhaul BAO controller. Now supports proper workload execution.
    This includes running the workload while gathering EXPLAIN ANALYZE
    output while still training the BAO model.

--- pg-bao/postgres-bao-ctl.py
+++ pg-bao/postgres-bao-ctl.py
@@ -5,14 +5,21 @@ import functools
 import getpass
 import json
 import os
-import sqlite3
-from typing import Any, List
+import sys
+from typing import Any, Dict, List
 
+import numpy as np
 import psycopg2 as pg
 
 SELECT_QUERY_PREFIX = "select"
 EXPLAIN_QUERY_PREFIX = "explain"
 COMMENT_PREFIX = "--"
+
+BAO_NUM_ARMS = int(os.environ.get("BAO_NUM_ARMS", 5))
+
+
+def message(*contents: str) -> None:
+    print(*contents, file=sys.stderr)
 
 
 def is_workload_query(query: str) -> bool:
@@ -32,6 +39,46 @@ def simplify_query(query: str) -> str:
         return query
 
 
+class BaoQuery:
+    def __init__(self, query: str):
+        simplified = simplify_query(query)
+        self.pure_query = simplified
+        self.explain_query = "EXPLAIN (ANALYZE, FORMAT JSON) " + simplified
+
+    def query(self) -> str:
+        return self.pure_query
+
+    def explain(self) -> str:
+        return self.explain_query
+
+    def run(self, cursor: "pg.cursor") -> None:
+        cursor.execute(self.pure_query)
+
+    def run_analyze(self, cursor: "pg.cursor") -> Dict[Any, Any]:
+        cursor.execute(self.explain_query)
+        return cursor.fetchone()[0]
+
+
+class BaoCtl:
+    def __init__(self, cursor: "pg.cursor"):
+        self.cursor = cursor
+
+    def on(self, planning=True, learning=True) -> None:
+        self.cursor.execute("SET enable_bao='on'")
+        self.cursor.execute("SET enable_bao_rewards='on'")
+        self.cursor.execute("SET enable_bao_selection='on'")
+        self.cursor.execute(f"SET bao_num_arms={BAO_NUM_ARMS}")
+
+    def off(self) -> None:
+        self.cursor.execute("SET enable_bao='off'")
+
+    def no_selection(self) -> None:
+        self.on(planning=False)
+
+    def no_learning(self) -> None:
+        self.on(learning=False)
+
+
 def read_raw_workload(workload: str, *, simplify=False) -> List[str]:
     contents = []
     with open(workload, "r") as workload_file:
@@ -44,25 +91,37 @@ def read_raw_workload(workload: str, *, simplify=False) -> List[str]:
         return without_comments
 
 
-def execute_single_query(cursor: "pg.cursor", query: str, gather_results=True) -> Any:
-    cursor.execute(query)
-    if gather_results:
-        return json.dumps(cursor.fetchall())
+def execute_single_query(cursor: "pg.cursor", query: str, workload=True) -> Any:
+    if not workload:
+        cursor.execute(query)
+        return
+
+    # At this point, we need to run a workload query. Since BAO appears to be
+    # unable to learn from EXPLAIN ANALYZE queries, but we are mainly interested
+    # in the query plans, we need to execute the query twice:
+    # The first execution runs the query "as is" with BAO enabled, to enable it
+    # to learn from the query. The second execution is the actual EXPLAIN
+    # ANALYZE RUN with learning disabled (just to be sure).
+    bao_ctl = BaoCtl(cursor)
+    bao_query = BaoQuery(query)
+
+    bao_ctl.on()
+    bao_query.run(cursor)
+
+    bao_ctl.no_learning()
+    return json.dumps(bao_query.run_analyze(cursor))
 
 
 def bao_retrain():
-    os.system("cd bao/bao_server && python3 baoctl.py --retrain")
+    os.system("""cd bao/bao_server && CUDA_VISIBLE_DEVICES="" python3 baoctl.py --retrain""")
     os.system("sync")
 
 
 def bao_reset():
-    conn = sqlite3.connect("bao/bao_server/bao.db")
-    cursor = conn.cursor()
-    cursor.execute("DELETE FROM experience")
-    cursor.execute("DELETE FROM experimental_query")
-    cursor.execute("DELETE FROM experience_for_experimental")
-    conn.commit()
+    os.system("./postgres-bao-shutdown.sh")
+    os.system("rm bao/bao_server/bao.db")
     os.system("rm -rf bao/bao_server/bao_*_model")
+    os.system("./postgres-bao-start.sh")
 
 
 def run_workload_chunked(workload: List[str], *, conn: "pg.connection", chunk_size: int) -> List[str]:
@@ -77,32 +136,25 @@ def run_workload_chunked(workload: List[str], *, conn: "pg.connection", chunk_si
     current_chunk_size = 0
     for query in workload:
         if current_chunk_size == chunk_size:
-            action_workload.append(bao_retrain)
+            action_workload.append((False, bao_retrain))
             current_chunk_size = 0
 
         if is_workload_query(query):
-            action_workload.append(functools.partial(execute_single_query, cursor, query))
+            action_workload.append((query, functools.partial(execute_single_query, cursor, query)))
             current_chunk_size += 1
         else:
-            action_workload.append(functools.partial(execute_single_query, cursor, query, False))
+            action_workload.append((query, functools.partial(execute_single_query, cursor, query, False)))
 
     # now, execute the actual workload
     results = []
-    for action in action_workload:
+    for description, action in action_workload:
+        if description:
+            message("Now running query", description)
+        else:
+            message("Retraining the model")
         results.append(action())
 
     return [res + "\n" for res in results if res is not None]
-
-
-def run_workload_complete(workload: List[str], *, conn: "pg.connection") -> List[str]:
-    cursor = conn.cursor()
-    results = []
-    for query in workload:
-        cursor.execute(query)
-        if is_workload_query(query):
-            results.append(json.dumps(cursor.fetchall()))
-            results.append("")
-    return [res + "\n" for res in results]
 
 
 def write_results_file(results: List[str], out: str) -> None:
@@ -120,10 +172,10 @@ def main():
         description="Utility to run an SQL workload and control the BAO server.")
     arg_grp = parser.add_mutually_exclusive_group()
     arg_grp.add_argument("--run-workload", action="store_true",
-                         help="Run a specific workload on the BAO/Postgres instance. How the BAO features should used has to be specified in the workload file, e.g. via SET enable_bao='on';")
+                         help="Run a specific workload on the BAO/Postgres instance. This enables both the learning, as well as the planning feature of BAO.")
     arg_grp.add_argument("--retrain-bao", action="store_true",
                          help="Don't run any workload, simply train the BAO model.")
-    arg_grp.add_argument("--reset-bao", action="store_true", help="Delete everything BAO has learned so far.")
+    arg_grp.add_argument("--reset-bao", action="store_true", help="Delete everything BAO has learned so far. This will restart both Postgres, as well as the BAO server.")
     parser.add_argument("--workload", "-w", action="store",
                         help="File to load the workload from. Only used if --run-workload is set.")
     parser.add_argument("--retrain", "-r", metavar="N", action="store", type=int, default=-1,
@@ -151,7 +203,7 @@ def main():
         if args.retrain >= 0:
             results = run_workload_chunked(workload, conn=postgres, chunk_size=args.retrain)
         elif args.run_workload:
-            results = run_workload_complete(workload, conn=postgres)
+            results = run_workload_chunked(workload, conn=postgres, chunk_size=np.inf)
         result_writer(results)
     elif args.retrain_bao:
         bao_retrain()
