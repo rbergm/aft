d02b47e52f43aee01e7e515037a7e587cd527eb4
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2022-01-26 14:13:48 +0100

    Support random workload subsets for BAO training

--- pg-bao/postgres-bao-ctl.py
+++ pg-bao/postgres-bao-ctl.py
@@ -6,10 +6,11 @@ import getpass
 import json
 import math
 import os
+import random
 import signal
 import sys
 import timeit
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Tuple, Union
 
 import numpy as np
 import pandas as pd
@@ -23,6 +24,7 @@ BAO_NUM_ARMS = int(os.environ.get("BAO_NUM_ARMS", 5))
 
 
 def message(*contents: str) -> None:
+    """print for stderr."""
     print(*contents, file=sys.stderr)
 
 
@@ -32,6 +34,7 @@ def is_workload_query(query: str) -> bool:
 
 
 def simplify_query(query: str) -> str:
+    """Deletes all leading statements of a query up to the first SELECT statement (e.g. EXPLAIN)."""
     if not is_workload_query(query):
         return query
 
@@ -44,6 +47,7 @@ def simplify_query(query: str) -> str:
 
 
 class BaoQuery:
+    """Provides a number of utilities to conveniently run SQL queries on BAO instances."""
     def __init__(self, query: str):
         simplified = simplify_query(query)
         self.pure_query = simplified
@@ -64,22 +68,29 @@ class BaoQuery:
 
 
 class BaoCtl:
+    """Enables control of the BAO training/planning mode."""
     def __init__(self, cursor: "pg.cursor"):
         self.cursor = cursor
 
     def on(self, planning=True, learning=True) -> None:
+        """Enables BAO according to the parameters given."""
+        selection = 'on' if planning else 'off'
+        rewards = 'on' if learning else 'off'
         self.cursor.execute("SET enable_bao='on'")
-        self.cursor.execute("SET enable_bao_rewards='on'")
-        self.cursor.execute("SET enable_bao_selection='on'")
+        self.cursor.execute(f"SET enable_bao_rewards='{rewards}'")
+        self.cursor.execute(f"SET enable_bao_selection='{selection}'")
         self.cursor.execute(f"SET bao_num_arms={BAO_NUM_ARMS}")
 
     def off(self) -> None:
+        """Disables BAO."""
         self.cursor.execute("SET enable_bao='off'")
 
     def no_selection(self) -> None:
+        """Disables BAO planning (i.e. only the Postgres optimizer runs), leaving everything else on."""
         self.on(planning=False)
 
     def no_learning(self) -> None:
+        """Disables BAO learning (i.e. executed queries are not used to improve the BAO model), leaving everything else on."""
         self.on(learning=False)
 
 
@@ -91,7 +102,19 @@ def read_raw_workload(workload: str) -> List[str]:
     return [query for query in contents if not query.startswith(COMMENT_PREFIX)]
 
 
-def execute_single_query(cursor: "pg.cursor", query: str, workload=True) -> Any:
+def select_queries_for_training(workload: List[str], training_fraction: float) -> List[Tuple[str, bool]]:
+    """Chooses a fraction of the workload queries for training, annotating each query accordingly."""
+    n_queries = len(workload)
+    num_training_queries = math.ceil(training_fraction * n_queries)
+    training_idx = random.sample(range(n_queries), k=num_training_queries)
+    query_idx = np.full(n_queries, fill_value=False, dtype=bool)
+    query_idx[training_idx] = True
+    annotated_queries = zip(workload, query_idx)
+    return list(annotated_queries)
+
+
+def execute_single_query(cursor: "pg.cursor", query: str, *, workload=True, for_training=True) -> Any:
+    """Runs a query, leveraging BAO functionality."""
     if not workload:
         cursor.execute(query)
         return
@@ -105,7 +128,7 @@ def execute_single_query(cursor: "pg.cursor", query: str, workload=True) -> Any:
     bao_ctl = BaoCtl(cursor)
     bao_query = BaoQuery(query)
 
-    bao_ctl.on()
+    bao_ctl.on(learning=for_training)
     bao_query.run(cursor)
 
     bao_ctl.no_learning()
@@ -124,26 +147,45 @@ def bao_reset():
     os.system("./postgres-bao-start.sh")
 
 
-def run_workload_chunked(workload: List[str], *, conn: "pg.connection", chunk_size: int) -> List[str]:
+def run_workload_chunked(workload: Union[List[str], List[Tuple[str, bool]]], *, conn: "pg.connection", training_chunk_size: int) -> List[str]:
+    """Executes a given workload on the BAO instance."""
+
     # when executing a workload in chunks (i.e. with retraining bao every N queries), we need to make
     # sure that the chunks do not consist of "meta-queries" (i.e. queries that modify the postgres behaviour rather
     # than EXPLAINing or SELECTing). Therefore we split the workload into segments of query chunks and segments of
     # meta statements, all while retaining the original query execution order.
+    if not workload:
+        return []
+
+    # each query should be annotated by whether it should be used for BAO training. If this not the case already,
+    # we assume the missing queries to  be suited for training
+    workload = list(map(lambda query: query if type(query) == tuple else (query, True), workload))
+
     cursor = conn.cursor()
 
     # first up, insert retraining actions into the workload
     action_workload = []
     current_chunk_size = 0
-    for query in workload:
-        if current_chunk_size == chunk_size:
-            action_workload.append((False, bao_retrain))
+    for query, use_for_training in workload:
+
+        # if our current chunk of training queries is full, we insert a training action
+        if current_chunk_size == training_chunk_size:
+            action_workload.append((False, bao_retrain))  # False indicates no specific description
             current_chunk_size = 0
 
         if is_workload_query(query):
-            action_workload.append((query, functools.partial(execute_single_query, cursor, query)))
-            current_chunk_size += 1
+            # workload queries can potentially influence the training set and require updating of the current
+            # training chunk
+            action = functools.partial(execute_single_query, cursor, query, for_training=use_for_training)
+            action_workload.append((query, action))
+
+            # if the query should be used for training, we update the size of the training chunk
+            if use_for_training:
+                current_chunk_size += 1
+                message(f"Using query {query} for training")
         else:
-            action_workload.append((query, functools.partial(execute_single_query, cursor, query, False)))
+            # non-workload queris (e.g. SET statements) are not considered as part of the training chunk
+            action_workload.append((query, functools.partial(execute_single_query, cursor, query, workload=False)))
 
     # now, execute the actual workload
     results = []
@@ -180,6 +222,11 @@ def write_runtime(args: argparse.Namespace, runtime: int) -> None:
     timing_df.to_csv(out_file, index=False)
 
 
+def write_training_status(workload: List[Tuple[str, bool]], out: str) -> None:
+    training_df = pd.DataFrame(workload, columns=["query", "training"])
+    training_df.to_csv(out, index=False)
+
+
 def main():
     parser = argparse.ArgumentParser(
         description="Utility to run an SQL workload and control the BAO server.")
@@ -193,13 +240,13 @@ def main():
                         help="File to load the workload from. Only used if --run-workload is set.")
     parser.add_argument("--retrain", "-r", metavar="N", action="store", type=int, default=-1,
                         help="Retrain the BAO model every N queries. Only used if --run-workload is set.")
+    parser.add_argument("--training-fraction", action="store", type=float, help="Fraction of the workload queries to be used as training data. By default, all queries will be used for training.")
+    parser.add_argument("--training-out", action="store", help="File to document which queries were used for training.")
     parser.add_argument("--output", "-o", action="store",
                         help="File to write the workload results to.")
     parser.add_argument("--pg-connect", "-c", metavar="connect", action="store", help="Custom Postgres connect string")
     parser.add_argument("--timing", "-t", action="store_true", help="Measure the execution time of this script")
     parser.add_argument("--timing-out", action="store", help="Write timing information to the given file")
-
-    signal.signal(signal.SIGINT, lambda: sys.exit(1))
 
     args = parser.parse_args()
     if not (args.run_workload or args.retrain_bao or args.reset_bao):
@@ -209,20 +256,39 @@ def main():
         parser.error(
             "No workload given. Use --workload to specify the source file.")
 
+    signal.signal(signal.SIGINT, lambda: sys.exit(1))
+
     if args.timing:
         start_time = timeit.default_timer()
 
     if args.run_workload:
+        # workload execution may be influenced by a number of additional arguments, the actions will be applied accordingly
+
+        # connect to the Postgres instance
         pg_connect = args.pg_connect if args.pg_connect else "dbname=imdb user={u} host=localhost".format(u=getpass.getuser())
         postgres = pg.connect(pg_connect)
-        result_writer = functools.partial(write_results_file, out=args.output) if args.output else write_results_stdout
+
+        # prepare the workload
         workload = read_raw_workload(args.workload)
+        if args.training_fraction:
+            workload = select_queries_for_training(workload, args.training_fraction)
+
+        # prepare the computation
+        chunk_size = args.retrain if args.retrain >= 0 else np.inf
         results = []
-        if args.retrain >= 0:
-            results = run_workload_chunked(workload, conn=postgres, chunk_size=args.retrain)
-        elif args.run_workload:
-            results = run_workload_chunked(workload, conn=postgres, chunk_size=np.inf)
+
+        # the actual execution
+        results = run_workload_chunked(workload, conn=postgres, training_chunk_size=chunk_size)
+
+        # store results
+        result_writer = functools.partial(write_results_file, out=args.output) if args.output else write_results_stdout
         result_writer(results)
+
+        # store additional data
+        if args.training_out:
+            write_training_status(workload, args.training_out)
+
+        # teardown
         postgres.close()
     elif args.retrain_bao:
         bao_retrain()
