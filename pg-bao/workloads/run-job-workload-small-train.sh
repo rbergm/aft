9bdcfbd221c9bd5503fc438476884e1df9bcadeb
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2022-01-31 09:46:22 +0100

    Workload pipeline for pg-bao with small training sets

---
+++ pg-bao/workloads/run-job-workload-small-train.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+REPETITIONS=3
+
+./postgres-bao-ctl.py --reset-bao
+
+for ((run=1 ; run <= $REPETITIONS ; run++))
+do
+	echo "Run $run"
+	echo ".. Running workload"
+	./postgres-bao-ctl.py --run-workload --workload workloads/job-full.sql -o workloads/job-small-train-run$run.out --training-fraction 0.3 --training-out workloads/job-small-train-run$run.csv 2>job-small-train-run$run.log
+	echo ".. Retraining model"
+	./postgres-bao-ctl.py --retrain-bao --timing --timing-out workloads/job-small-train-timing-run$run.csv
+done
