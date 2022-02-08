6f0409035ba82d900d3e640de62827bdae459641
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2022-02-08 10:36:23 +0100

    Modify UES jitter pipeline to operate on implicit/explicit workloads

--- ues/workloads/runtime-jitter-pipeline.sh
+++ ues/workloads/runtime-jitter-pipeline.sh
@@ -2,20 +2,18 @@
 
 REPETITIONS=5
 
+if [ -z "$1" ]
+then
+	echo "Setting mode to default implicit"
+	MODE="implicit"
+else
+	MODE="$1"
+fi
+
 for ((run = 1; run <= $REPETITIONS ; run++))
 do
-	if [ "$1" = "-clean" ]
-	then
-		echo "Setting up new Docker image"
-		./ues-docker-reset.sh
-		./ues-docker-create.sh
-		./ues-docker-start.sh
-		./ues-docker-connect.sh << EOF
-exit
-EOF
-	fi
 	echo "Run $run"
 	# we need to explicitly use the UNIX utility here, to prevent the bash-builtin tool from shadowing it
-	/usr/bin/time --append -f "$(date -u +%s),$run,%E" -o timing$1.csv ./ues-docker-run-sql.sh workloads/job-implicit.sql /tmp/job-implicit$1-run$run.out
+	/usr/bin/time --append -f "$(date -u +%s),$run,%E" -o workloads/timing-$MODE.csv ./ues-docker-run-sql.sh workloads/job-$MODE.sql /tmp/job-$MODE-run$run.out
 done
 
