25519ef0b64608f4f5efcd334e30d2925a803923
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2022-02-10 14:01:32 +0100

    Make pg-bao jitter analysis more flexible

--- pg-bao/workloads/runtime-jitter-pipeline.sh
+++ pg-bao/workloads/runtime-jitter-pipeline.sh
@@ -1,12 +1,41 @@
 #!/bin/bash
 
+# default argument values
 REPETITIONS=5
+CACHE_OPT=""
+ITERATIONS=3
+
+# parse provided command line args
+while [[ $# -gt 0 ]] ; do
+	case $1 in
+		-r|--repetitions)
+			REPETITIONS="$2"
+			shift
+			shift
+			;;
+		--no-cache)
+			CACHE_OPT="--no-cache"
+			shift
+			;;
+		-i|--iterations)
+			ITERATIONS="$2"
+			shift
+			shift
+			;;
+	esac
+done
+
+if [ "$1" = "--repetitions" ] ; then
+	REPETITIONS="$2"
+else
+	REPETITIONS=5
+fi
 
 for ((run = 1; run <= $REPETITIONS ; run++))
 do
 	echo "Run $run"
 	echo ".. Running workload"
 	# we need to explicitly use the UNIX utility here, to prevent the bash-builtin tool from shadowing it
-	/usr/bin/time --append -f "$(date -u +%s),$run,%E" -o workloads/timing$1.csv workloads/run-job-workload-full-train.sh --dummy
+	/usr/bin/time --append -f "$(date -u +%s),$run,%E" -o workloads/timing$1.csv workloads/run-job-workload-full-train.sh --dummy --repetitions $ITERATIONS $CACHE_OPT
 done
 
