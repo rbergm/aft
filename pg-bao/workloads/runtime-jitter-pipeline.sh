8d7c491f949b0b0254d64166d217e5b3a5b7e0a1
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2022-02-11 12:46:00 +0100

    Make runtime jitter pipeline output more verbose

--- pg-bao/workloads/runtime-jitter-pipeline.sh
+++ pg-bao/workloads/runtime-jitter-pipeline.sh
@@ -4,6 +4,7 @@
 REPETITIONS=5
 CACHE_OPT=""
 ITERATIONS=3
+TIMING_SUFFIX="-full-train"
 
 # parse provided command line args
 while [[ $# -gt 0 ]] ; do
@@ -15,6 +16,7 @@ while [[ $# -gt 0 ]] ; do
 			;;
 		--no-cache)
 			CACHE_OPT="--no-cache"
+			TIMING_SUFFIX="-full-train-no-cache"
 			shift
 			;;
 		-i|--iterations)
@@ -36,6 +38,6 @@ do
 	echo "Run $run"
 	echo ".. Running workload"
 	# we need to explicitly use the UNIX utility here, to prevent the bash-builtin tool from shadowing it
-	/usr/bin/time --append -f "$(date -u +%s),$run,%E" -o workloads/timing$1.csv workloads/run-job-workload-full-train.sh --dummy --repetitions $ITERATIONS $CACHE_OPT
+	/usr/bin/time --append -f "$(date -u +%s),$run,%E" -o workloads/timing$TIMING_SUFFIX.csv workloads/run-job-workload-full-train.sh --dummy --repetitions $ITERATIONS $CACHE_OPT
 done
 
